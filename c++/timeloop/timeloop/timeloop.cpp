8dd61567127e4302b86a6157da5fc687bad3a0d2
Karlssn <johnmartink@hotmail.com>
2019-11-28 14:12:40 +0100

    aah and timeloop completed

---
+++ c++/timeloop/timeloop/timeloop.cpp
@@ -0,0 +1,16 @@
+// timeloop.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	for (size_t i = 1; i <=n; i++)
+	{
+		cout << i << " Abracadabra" << endl;
+	}
+}
